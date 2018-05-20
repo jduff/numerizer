2e62376bdd2b9893e943be8a6a88be444bde4bc8
Krithik Rao <krdevmail@gmail.com>
2018-05-19 20:45:38 -0500

    Removed Loops using Hashes
    Previously, there were a number of loops that could potentially cause
    problems ... performance wise and just logic wise. Now that has been
    simplified by the usage of hash. Instead of looping over nested arrays
    through each and every possible number, you simply gsub checking for all
    words, and then use the match as a key in a hash, returning you the
    appropriate number. This way, you don't use gsub in a nested loop,
    repeatedly scanning over the string an unnecessary amount of times.
    However, this fix does change the logic a little bit, and may be slighty
    more complex when dealing with edge cases, like with 'second' and
    'quarter'. It is to be seen how this can be refactored.

--- lib/numerizer.rb
+++ lib/numerizer.rb
@@ -16,199 +16,217 @@ require 'numerizer/version'
 
 class Numerizer
 
-  DIRECT_NUMS = [
-    ['eleven', '11'],
-    ['twelve', '12'],
-    ['thirteen', '13'],
-    ['fourteen', '14'],
-    ['fifteen', '15'],
-    ['sixteen', '16'],
-    ['seventeen', '17'],
-    ['eighteen', '18'],
-    ['nineteen', '19'],
-    ['ninteen', '19'], # Common mis-spelling
-    ['zero', '0'],
-    ['ten', '10'],
-    ['\ba\b', '1']
-  ]
+  DIRECT_NUMS = {
+    'eleven' => '11',
+    'twelve' => '12',
+    'thirteen' => '13',
+    'fourteen' => '14',
+    'fifteen' => '15',
+    'sixteen' => '16',
+    'seventeen' => '17',
+    'eighteen' => '18',
+    'nineteen' => '19',
+    'ninteen' => '19',
+    'zero' => '0',
+    'ten' => '10',
+  }
 
-  SINGLE_NUMS = [
-    ['one', 1],
-    ['two', 2],
-    ['three', 3],
-    ['four', 4],
-    ['five', 5],
-    ['six', 6],
-    ['seven', 7],
-    ['eight', 8],
-    ['nine', 9]
-  ]
+  SINGLE_NUMS = {
+    'one' => 1,
+    'two' => 2,
+    'three' => 3,
+    'four' => 4,
+    'five' => 5,
+    'six' => 6,
+    'seven' => 7,
+    'eight' => 8,
+    'nine' => 9
+  }
 
-  TEN_PREFIXES = [
-    ['twenty', 20],
-    ['thirty', 30],
-    ['forty', 40],
-    ['fourty', 40], # Common misspelling
-    ['fifty', 50],
-    ['sixty', 60],
-    ['seventy', 70],
-    ['eighty', 80],
-    ['ninety', 90]
-  ]
+  TEN_PREFIXES = {
+    'twenty' => 20,
+    'thirty' => 30,
+    'forty' => 40,
+    'fourty' => 40,
+    'fifty' => 50,
+    'sixty' => 60,
+    'seventy' => 70,
+    'eighty' => 80,
+    'ninety' => 90
+  }
 
-  BIG_PREFIXES = [
-    ['hundred', 100],
-    ['thousand', 1000],
-    ['million', 1_000_000],
-    ['billion', 1_000_000_000],
-    ['trillion', 1_000_000_000_000],
-  ]
+  BIG_PREFIXES = {
+    'hundred' => 100,
+    'thousand' => 1000,
+    'million' => 1_000_000,
+    'billion' => 1_000_000_000,
+    'trillion' => 1_000_000_000_000,
+  }
 
-  FRACTIONS = [
-    ['hal(f|ves)', 2],
-    ['quarter(s)?', 4],
-  ]
+  FRACTIONS = {
+    'half' => 2,
+    'halves' => 2,
+    'quarter' => 4,
+    'quarters' => 4
+  }
 
-  ORDINALS = [
-    ['first', 1],
-    ['second', 2],
-  ]
+  ORDINALS = {
+    'first' => 1,
+    'second' => 2,
+  }
 
-  SINGLE_ORDINAL_FRACTIONALS = [
-    ['third', 3],
-    ['fourth', 4],
-    ['fifth', 5],
-    ['sixth', 6],
-    ['seventh', 7],
-    ['eighth', 8],
-    ['ninth', 9],
-  ]
+  SINGLE_ORDINAL_FRACTIONALS = {
+    'third' => 3,
+    'fourth' => 4,
+    'fifth' => 5,
+    'sixth' => 6,
+    'seventh' => 7,
+    'eighth' => 8,
+    'ninth' => 9,
+  }
 
-  DIRECT_ORDINAL_FRACTIONALS = [
-    ['tenth', '10'],
-    ['eleventh', '11'],
-    ['twelfth', '12'],
-    ['thirteenth', '13'],
-    ['fourteenth', '14'],
-    ['fifteenth', '15'],
-    ['sixteenth', '16'],
-    ['seventeenth', '17'],
-    ['eighteenth', '18'],
-    ['nineteenth', '19'],
-    ['twentieth', '20'],
-    ['thirtieth', '30'],
-    ['fourtieth', '40'],
-    ['fiftieth', '50'],
-    ['sixtieth', '60'],
-    ['seventieth', '70'],
-    ['eightieth', '80'],
-    ['ninetieth', '90']
-  ]
+  DIRECT_ORDINAL_FRACTIONALS = {
+    'tenth' => '10',
+    'eleventh' => '11',
+    'twelfth' => '12',
+    'thirteenth' => '13',
+    'fourteenth' => '14',
+    'fifteenth' => '15',
+    'sixteenth' => '16',
+    'seventeenth' => '17',
+    'eighteenth' => '18',
+    'nineteenth' => '19',
+    'twentieth' => '20',
+    'thirtieth' => '30',
+    'fourtieth' => '40',
+    'fiftieth' => '50',
+    'sixtieth' => '60',
+    'seventieth' => '70',
+    'eightieth' => '80',
+    'ninetieth' => '90'
+  }
 
-  ALL_ORDINALS = ORDINALS + SINGLE_ORDINAL_FRACTIONALS + DIRECT_ORDINAL_FRACTIONALS
-  ALL_FRACTIONS = FRACTIONS + (SINGLE_ORDINAL_FRACTIONALS + DIRECT_ORDINAL_FRACTIONALS).map {|tp| [tp[0] + '(s)?', tp[1]] }
-  ONLY_PLURAL_FRACTIONS = FRACTIONS + (SINGLE_ORDINAL_FRACTIONALS + DIRECT_ORDINAL_FRACTIONALS).map {|tp| [tp[0] + 's', tp[1]] }
+  ALL_ORDINALS = ORDINALS.merge(SINGLE_ORDINAL_FRACTIONALS).merge(DIRECT_ORDINAL_FRACTIONALS)
+  ONLY_PLURAL_FRACTIONS = FRACTIONS.merge((SINGLE_ORDINAL_FRACTIONALS.merge(DIRECT_ORDINAL_FRACTIONALS)).inject({ }) {|h, (k,v)| h[k + 's'] = v ; h})
+  ALL_FRACTIONS = ONLY_PLURAL_FRACTIONS.merge(SINGLE_ORDINAL_FRACTIONALS).merge(DIRECT_ORDINAL_FRACTIONALS)
 
-  ALL_ORDINALS_REGEX = ALL_ORDINALS.reduce { |a,b| a[0] + '|' + b[0] }
+  DIRECT_SINGLE_NUMS = DIRECT_NUMS.merge(SINGLE_NUMS)
+  ORDINAL_SINGLE = ORDINALS.merge(SINGLE_ORDINAL_FRACTIONALS)
+
+  ALL_ORDINALS_REGEX = Regexp.union(ALL_ORDINALS.keys)
+
+  PRONOUNS = /i|you|he|she|we|it|you|they|to|the/i
 
 
   def self.numerize(string, ignore: [], bias: :none)
     string = string.dup
     ignore = ignore.map(&:downcase).to_set
 
-    # preprocess
-    string.gsub!(/ +|([^\d])-([^\d])/, '\1 \2') # will mutilate hyphenated-words
-    string.gsub!(/\ba$/, '') && string.rstrip! # doesn't make sense for an 'a' at the end to be a 1
-
-    # easy/direct replacements
-    (DIRECT_NUMS + SINGLE_NUMS).each do |dn|
-      next if ignore.include? dn[0].downcase 
-      string.gsub!(/(^|\W)#{dn[0]}(?=$|\W)/i, '\1<num>' + dn[1].to_s) 
-    end
-
-    # ten, twenty, etc.
-    TEN_PREFIXES.each do |tp|
-      next if ignore.include? tp[0].downcase 
-      SINGLE_NUMS.each do |dn|
-        next if ignore.include? dn[0].downcase 
-        string.gsub!(/(^|\W)#{tp[0]}#{dn[0]}(?=$|\W)/i, '\1<num>' + (tp[1] + dn[1]).to_s)
-      end
-      (ORDINALS + SINGLE_ORDINAL_FRACTIONALS).each do |dn|
-        next if ignore.include? dn[0].downcase 
-        string.gsub!(/(^|\W)#{tp[0]}(\s)?#{dn[0]}(?=$|\W)/i, '\1<num>' + (tp[1] + dn[1]).to_s + dn[0][-2, 2])
-      end
-      string.gsub!(/(^|\W)#{tp[0]}(?=$|\W)/i, '\1<num>' + tp[1].to_s)
-    end
-
-    # handle fractions
-    # only plural fractions if ordinal mode
-    if bias == :ordinal
-      fractionals = ONLY_PLURAL_FRACTIONS 
-    else
-      fractionals = ALL_FRACTIONS
-    end
-
-    fractionals.each do |tp|
-      next unless ignore.select {|x| /#{tp[0]}/ =~ x } .empty?
-      string.gsub!(/a #{tp[0]}(?=$|\W)/i, '<num>1/' + tp[1].to_s)
-      # TODO : Find Noun Distinction for Quarter
-      # Handle Edge Case with Quarter
-      if bias == :fractional
-        string.gsub!(/(^|\W)#{tp[0]}(?=$|\W)/i, '/' + tp[1].to_s)
-      elsif /quarter/ =~ tp[0] then
-        string.gsub!(/(?:\w*)(^|\W)#{tp[0]}(?=$|\W)/i) do |match|
-          if (match =~ /^(i|you|he|she|we|it|you|they|to|the)/i) == nil then 
-            match.gsub!(/(^|\W)#{tp[0]}/, '/' + tp[1].to_s)
-          end
-          match
-        end
-      else
-        string.gsub!(/(?<!the)\s#{tp[0]}(?=$|\W)/i, '/' + tp[1].to_s)
-      end
-    end
-
-    ALL_ORDINALS.each do |on|
-      break if bias == :fractional # shouldn't be necessary but saves cycles
-      next if ignore.include? on[0].downcase 
-      if on[0] == 'second' and bias != :ordinal then
-        # We don't want to substitute second preceeded by second or another ordinal
-        string.gsub!(/\w*(?<!second)(^|\W)#{on[0]}(?=$|\W)/i) do |match| 
-          unless match =~ /^(\d|#{ALL_ORDINALS_REGEX})/ then
-            match.gsub!(/(^|\W)#{on[0]}(?=$|\W)/i, '\1<num>' + on[1].to_s + on[0][-2, 2])
-          end
-          match
-        end
-      else
-        string.gsub!(/(^|\W)#{on[0]}(?=$|\W)/i, '\1<num>' + on[1].to_s + on[0][-2, 2])
-      end
-    end
-
-    # evaluate fractions when preceded by another number
-    string.gsub!(/(\d+)(?: | and |-)+(<num>|\s)*(\d+)\s*\/\s*(\d+)/i) { ($1.to_f + ($3.to_f/$4.to_f)).to_s }
-
-    # fix unpreceeded fractions
-    string.gsub!(/(?:^|\W)\/(\d+)/, '1/\1')
-    string.gsub!(/(?<=[a-zA-Z])\/(\d+)/, ' 1/\1')
-
-    # hundreds, thousands, millions, etc.
-    BIG_PREFIXES.each do |bp|
-      next if ignore.include? bp[0].downcase 
-      string.gsub!(/(?:<num>)?(\d*) *#{bp[0]}/i) { $1.empty? ? bp[1] : '<num>' + (bp[1] * $1.to_i).to_s }
-      andition(string)
-    end
-
+    preprocess(string)
+    sub_numerals(string, ignore)
+    sub_fractions(string, ignore, bias)
+    sub_ordinals(string, ignore, bias)
+    cleanup_fractions(string)
+    sub_bigprefs(string, ignore)
     andition(string)
+    sub_halves(string, ignore)
 
-    # always substitute halfs
-    unless ignore.include? 'half' then
-      string.gsub!(/\bhalf\b/i, '1/2')
-    end
-
+    #Strip Away Added Num Tags
     string.gsub(/<num>/, '')
   end
 
   class << self
     private
+
+    def preprocess(string)
+      string.gsub!(/ +|([^\d])-([^\d])/, '\1 \2') # will mutilate hyphenated-words
+      string.gsub!(/\ba$/, '') && string.rstrip! # doesn't make sense for an 'a' at the end to be a 1
+    end
+
+    def sub_numerals(string, ignore)
+      # easy/direct replacements
+      dir_single_nums = (DIRECT_SINGLE_NUMS).keys.reject { |x| ignore.include? x }
+      string.gsub!(/(^|\W)(#{Regexp.union(dir_single_nums)})(?=$|\W)/i) { $1 + '<num>' + DIRECT_SINGLE_NUMS[$2].to_s} 
+      string.gsub!(/(^|\W)\ba\b(?=$|\W)/i, '\1<num>' + 1.to_s)
+
+      # ten, twenty, etc.
+      ten_prefs = Regexp.union(TEN_PREFIXES.keys.reject { |x| ignore.include? x })
+      single_nums = Regexp.union(SINGLE_NUMS.keys.reject { |x| ignore.include? x })
+      single_ords = Regexp.union(ORDINAL_SINGLE.keys.reject { |x| ignore.include? x })
+      string.gsub!(/(^|\W)(#{ten_prefs})(#{single_nums})(?=$|\W)/i) { $1 + '<num>' + (TEN_PREFIXES[$2] + SINGLE_NUMS[$3]).to_s}
+      string.gsub!(/(^|\W)(#{ten_prefs})(\s)?(#{single_ords})(?=$|\W)/i) { $1 + '<num>' + (TEN_PREFIXES[$2] + ORDINAL_SINGLE[$4]).to_s + $4[-2, 2]}
+      string.gsub!(/(^|\W)(#{ten_prefs})(?=$|\W)/i) { $1 + '<num>' + TEN_PREFIXES[$2].to_s}
+    end
+
+    def sub_fractions(string, ignore, bias)
+      # handle fractions
+      # only plural fractions if ordinal mode
+      if bias == :ordinal
+        fractionals = Regexp.union(ONLY_PLURAL_FRACTIONS.keys.reject{ |x| ignore.include? x }) 
+      else
+        fractionals = Regexp.union(ALL_FRACTIONS.keys.reject{ |x| ignore.include? x})
+      end
+
+      string.gsub!(/a (#{fractionals})(?=$|\W)/i) {'<num>1/' + ALL_FRACTIONS[$1].to_s}
+      # TODO : Find Noun Distinction for Quarter
+      if bias == :fractional
+        string.gsub!(/(^|\W)(#{fractionals})(?=$|\W)/i) {'/' + ALL_FRACTIONS[$2].to_s}
+      else
+        string.gsub!(/(\w*)(?<!the)(^|\W)(#{fractionals})(?=$|\W)/i) do |match|
+          prematch = $1 || ""
+          space = $2 || ""
+          fraction = $3
+          # HANDLES QUARTER EDGE CASES
+          if (!$3.start_with?('quarter') && space == ' ') || ($3.start_with?('quarter') && (prematch =~ /^(#{PRONOUNS})\b/) == nil) 
+              match = prematch + '/' + ALL_FRACTIONS[fraction].to_s 
+          end
+          match
+        end
+      end
+    end
+
+    def sub_ordinals(string, ignore, bias)
+      if bias != :fractionals
+        all_ords = Regexp.union(ALL_ORDINALS.keys.reject {|x| ignore.include?(x) || (x == 'second' && bias != :ordinal) })
+        if bias != :ordinal && !ignore.include?('second')
+          string.gsub!(/(\w*)(?<!second)(^|\W)second(?=$|\W)/i) do |match| 
+            prematch = $1
+            space = $2
+            if (prematch =~ /^(\d|#{ALL_ORDINALS_REGEX})/i) == nil
+              match = prematch + space + '<num>' + ALL_ORDINALS['second'].to_s + 'second'[-2, 2]
+            end
+            match
+          end
+        end
+        string.gsub!(/(^|\W)(#{all_ords})(?=$|\W)/i) { $1 + '<num>' + ALL_ORDINALS[$2].to_s + $2[-2, 2]}
+      end
+    end
+
+    def cleanup_fractions(string)
+      # evaluate fractions when preceded by another number
+      string.gsub!(/(\d+)(?: | and |-)+(<num>|\s)*(\d+)\s*\/\s*(\d+)/i) { ($1.to_f + ($3.to_f/$4.to_f)).to_s }
+      # fix unpreceeded fractions
+      string.gsub!(/(?:^|\W)\/(\d+)/, '1/\1')
+      string.gsub!(/(?<=[a-zA-Z])\/(\d+)/, ' 1/\1')
+    end
+
+    def sub_bigprefs(string, ignore)
+      # hundreds, thousands, millions, etc.
+      # big_prefs = Regexp.union(BIG_PREFIXES.keys.reject { |x| ignore.include? x })
+      BIG_PREFIXES.each do |k,v|
+        next if ignore.include? k.downcase 
+        string.gsub!(/(?:<num>)?(\d*) *#{k}/i) { $1.empty? ? v : '<num>' + (v * $1.to_i).to_s }
+        andition(string)
+      end
+    end
+
+    def sub_halves(string, ignore)
+      # always substitute halfs
+      unless ignore.include? 'half'
+        string.gsub!(/\bhalf\b/i, '1/2')
+      end
+    end
+
     def andition(string)
       sc = StringScanner.new(string)
       while(sc.scan_until(/<num>(\d+)( | and )<num>(\d+)(?=[^\w]|$)/i))
@@ -218,6 +236,7 @@ class Numerizer
         end
       end
     end
+
   end
 
 end
